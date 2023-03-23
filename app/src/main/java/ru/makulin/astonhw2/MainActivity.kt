e10e55c0ce47c302c2fae15ad0e571af10c38ad2
PavelMakulin <p.a.makulin@gmail.com>
2023-03-23 20:33:41 +0300

    Создал макет для задания 2.3

--- app/src/main/java/ru/makulin/astonhw2/MainActivity.kt
+++ app/src/main/java/ru/makulin/astonhw2/MainActivity.kt
@@ -9,36 +9,10 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-    private var counter = INIT_COUNT
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        initViews()
-    }
-
-    private fun initViews() {
-        with(binding) {
-            btnCount.setOnClickListener {
-                countUp()
-            }
-            btnSayHello.setOnClickListener {
-                val intent = Intent(this@MainActivity, HelloActivity::class.java)
-                intent.putExtra(INTENT_EXTRA_KEY, counter)
-                startActivity(intent)
-            }
-            tvCount.text = counter.toString()
-        }
-    }
-
-    private fun countUp() {
-        counter++
-        binding.tvCount.text = counter.toString()
-    }
-
-    companion object {
-        private const val INIT_COUNT = 0
-        const val INTENT_EXTRA_KEY = "key"
     }
 
 }

17824624382b77d7cf6764ad6ee83adac1e285d6
PavelMakulin <p.a.makulin@gmail.com>
2023-03-23 16:50:18 +0300

    Реализовал счетчик

--- app/src/main/java/ru/makulin/astonhw2/MainActivity.kt
+++ app/src/main/java/ru/makulin/astonhw2/MainActivity.kt
@@ -1,11 +1,38 @@
 package ru.makulin.astonhw2
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import ru.makulin.astonhw2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private var counter = INIT_COUNT
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initViews()
     }
+
+    private fun initViews() {
+        with(binding) {
+            btnCount.setOnClickListener {
+                countUp()
+            }
+            btnSayHello.setOnClickListener { }
+            tvCount.text = counter.toString()
+        }
+    }
+
+    private fun countUp() {
+        counter++
+        binding.tvCount.text = counter.toString()
+    }
+
+    companion object {
+        private const val INIT_COUNT = 0
+    }
+
 }
